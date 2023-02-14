cbfff888798b0fd235d6f267024a8d183f6e0451
Niall Mclaughlin <niall.mclaughlin99@outlook.com>
2023-02-14 20:46:24 +0000

    End Of Section 3

--- NZWalks/NZWalks.API/Models/DTO/Region.cs
+++ NZWalks/NZWalks.API/Models/DTO/Region.cs
@@ -1,4 +1,4 @@
-﻿namespace NZWalks.API.Models.Domain
+﻿namespace NZWalks.API.Models.DTO
 {
     public class Region
     {
@@ -7,7 +7,7 @@
         public string Name { get; set; }
 
         public string Code { get; set; }
-        
+
         public double Area { get; set; }
 
         public double Lat { get; set; }
@@ -15,8 +15,5 @@
         public double Long { get; set; }
 
         public long Population { get; set; }
-
-        //Navigation Property 
-        public IEnumerable<Walk> Walks { get; set; }
     }
 }
